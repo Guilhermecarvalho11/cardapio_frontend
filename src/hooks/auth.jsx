f3a998afecebb5c760a0eded4b1c788f94b2e718
Guilherme Carvalho <carvalho_guilhermegomes@hotmail.com>
2024-08-14 17:05:15 -0300

    saving user in the localStorage

--- src/hooks/auth.jsx
+++ src/hooks/auth.jsx
@@ -1,6 +1,6 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable react/prop-types */
-import { createContext, useContext, useState } from "react";
+import { createContext, useContext, useState, useEffect } from "react";
 import { api } from "../services/api";
 
 export const AuthContext = createContext({});
@@ -12,6 +12,9 @@ function AuthProvider({ children }) {
       const response = await api.post("/sessions", { email, password });
       const { user, token } = response.data;
 
+      localStorage.setItem("@cardapioon:user", JSON.stringify(user));
+      localStorage.setItem("@cardapioon:token", token);
+
       api.defaults.headers.authorization = `Bearer ${token}`;
       setData({ user, token });
     } catch (error) {
@@ -22,6 +25,20 @@ function AuthProvider({ children }) {
       }
     }
   }
+
+  useEffect(() => {
+    const token = localStorage.getItem("@cardapioon:token");
+    const user = localStorage.getItem("@cardapioon:user");
+
+    if (token && user) {
+      api.defaults.headers.authorization = `Bearar ${token}`;
+
+      setData({
+        token,
+        user: JSON.parse(user),
+      });
+    }
+  }, []);
   return (
     <AuthContext.Provider value={{ signIn, user: data.user }}>
       {children}
