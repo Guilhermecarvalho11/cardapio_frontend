76da68c935a76c93ac67e57b09141bd7fffd1d9d
Guilherme Carvalho <carvalho_guilhermegomes@hotmail.com>
2024-08-21 17:29:18 -0300

    refactoring componente NewDishs

--- src/pages/NewDishs/index.jsx
+++ src/pages/NewDishs/index.jsx
@@ -1,4 +1,11 @@
-import { AdminPower } from "../../components/AdminPower";
+import { MdNavigateBefore, MdOutlineFileUpload } from "react-icons/md";
+import { Button } from "../../components/Button";
+import { Footer } from "../../components/Footer";
+import { Header } from "../../components/Header";
+import { Input } from "../../components/Input";
+import { LinkButton } from "../../components/LinkButton";
+import { Container, Form } from "./styled";
+import { ButtonIMG } from "../../components/ButtonIMG";
 
 export function NewDishs() {
   const handleSave = () => {
@@ -6,20 +13,49 @@ export function NewDishs() {
   };
 
   return (
-    <AdminPower
-      title="Novo Prato"
-      // titleButtonImg="Selecionar imagem"
-      namePlaceholder="Ex: Salada Ceasar"
-      categoryOptions={[
-        { value: "Refeição", label: "Refeição" },
-        { value: "pratos Principais", label: "pratos Principais" },
-      ]}
-      ingredientsPlaceholder=""
-      pricePlaceholder="R$ 00,00"
-      descriptionPlaceholder="Fale brevemente sobre o prato, seus ingredientes e composição"
-      showDeleteButton={false}
-      titleButton="Salvar alterações"
-      onSave={handleSave}
-    />
+    <>
+      <Header />
+      <Container>
+        <LinkButton icons={MdNavigateBefore} title="voltar" to="/" />
+        <h1>Novo Prato</h1>
+        <Form>
+          <>
+            <ButtonIMG icons={MdOutlineFileUpload} />
+          </>
+          <>
+            <label htmlFor="name">Name</label>
+            <Input id="name" type="" placeholder={"Ex: salada Ceasar"} />
+          </>
+          <>
+            <label htmlFor="category">Categoria</label>
+            <select id="category" name="Categoria">
+              <option value="Refeição">Refeição</option>
+              <option value="Pratos principais">Pratos principais</option>
+            </select>
+          </>
+          <>
+            <label htmlFor="ingredients">Ingredientes</label>
+            <Input id="ingredients" type="text" placeholder={""} />
+          </>
+          <>
+            <label htmlFor="price">Preço</label>
+            <Input id="price" type="number" placeholder={"R$ 00,00"} />
+          </>
+          <>
+            <label htmlFor="description">Descrição</label>
+            <textarea
+              id="description"
+              placeholder={
+                "Fale brevemente sobre o prato, seus ingredientes e composição"
+              }
+            ></textarea>
+          </>
+          <div className="buttons">
+            <Button title={"Salvar alterações"} onClick={handleSave} />
+          </div>
+        </Form>
+      </Container>
+      <Footer />
+    </>
   );
 }
--- src/pages/NewDishs/styled.js
+++ src/pages/NewDishs/styled.js
@@ -1,27 +1,25 @@
 import styled from "styled-components";
 
-import { Link } from "react-router-dom";
-
-export const StyledLink = styled(Link)`
-  display: flex;
-  align-items: center;
-  text-decoration: none;
-`;
-
 export const Container = styled.div`
+  min-height: 100vh;
   width: 88%;
   display: flex;
   flex-direction: column;
   margin: 0 auto;
-  border: 1px solid red;
 
   > h1 {
     color: white;
     font-family: roboto;
+    font-size: 22px;
   }
+`;
+
+export const Form = styled.form`
+  display: flex;
+  flex-direction: column;
 
   > label {
-    color: white;
+    color: ${({ theme }) => theme.COLORS.LIGHT_400};
     font-family: roboto;
     font-size: 14px;
     margin-bottom: 12px;
@@ -29,5 +27,35 @@ export const Container = styled.div`
 
   > label:first-of-type {
     margin-top: 12px;
+  }
+
+  > select {
+    background-color: ${({ theme }) => theme.COLORS.BACKGROUND_900};
+    color: ${({ theme }) => theme.COLORS.LIGHT_400};
+    height: 48px;
+    margin-bottom: 12px;
+  }
+
+  > textarea {
+    background: ${({ theme }) => theme.COLORS.BACKGROUND_800};
+    color: ${({ theme }) => theme.COLORS.LIGHT_500};
+    width: 100%;
+    max-width: 100%;
+    min-height: 172px;
+  }
+
+  .buttons {
+    display: flex;
+    width: 100%;
+    justify-content: space-between;
+    gap: 20px;
+
+    > button {
+      background-color: ${({ theme }) => theme.COLORS.BACKGROUND_800};
+      width: 100%;
+      margin-top: 12px;
+      border-radius: 5px;
+      color: white;
+    }
   }
 `;
